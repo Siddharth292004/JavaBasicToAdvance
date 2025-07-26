7ba323051d497a7dd0bec3656911727921901e7a
Siddharth292004 <sidharth9509@gmail.com>
2025-07-27 00:19:16 +0530

    initial commit

---
+++ Chapter 1/Literals/program_01.java
@@ -0,0 +1,10 @@
+public class program_01{
+    public static void main(String [] args){
+        int i1 = 123456;
+        int i2 = 123_456;
+        System.out.println(i1 == i2);
+        System.out.println("i1 = "+i1);
+        System.out.println("i2 = "+i2);
+
+    }
+}
---
+++ Chapter 1/Literals/program_02.java
@@ -0,0 +1,19 @@
+public class program_02 {
+    public static void main(String [] args){
+        byte color = 1_2_3;
+        short yearsAnnoDomini = 2_016;
+        int socialSecurityNumber = 999_99_999;
+        long creditCardNumber =1234_5678_7894_34564L;
+        float PiFourDecimal = 3.14_15F;
+        double piTenDecimal = 3.14_15_92_65_35;
+
+        System.out.println(+color);
+        System.err.println(+yearsAnnoDomini);
+        System.out.println(+socialSecurityNumber);
+        System.out.println(+creditCardNumber);
+        System.out.println(+PiFourDecimal);
+        System.out.println(+piTenDecimal);
+
+
+    }
+}
