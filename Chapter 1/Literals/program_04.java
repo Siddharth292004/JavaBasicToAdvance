77ed4519bceb4fb40dd01474987a0a173d202b07
Siddharth292004 <sidharth9509@gmail.com>
2025-08-09 23:57:55 +0530

    initial commit

--- Chapter 1/Literals/program_04.java
+++
@@ -1,7 +0,0 @@
-public class program_04 {
- 
-    public static void main(String[] args) {
-        char heart = '\u2764';  // Unicode for ❤
-        System.out.println(Character.toString(heart));  // Prints ❤
-    }
-}
---
+++ Chapter_01/Literals/SymbolTest.java
@@ -0,0 +1,7 @@
+public class SymbolTest {
+    public static void main(String[] args) {
+        System.out.println("Heart: \u2764");
+        System.out.println("Arrow: \u2192");
+        System.out.println("Check: \u2713");
+    }
+}
