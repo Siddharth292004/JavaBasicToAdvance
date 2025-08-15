3393b2e860a456e12b87241e4fa07dfc313d7d29
Siddharth292004 <sidharth9509@gmail.com>
2025-08-15 14:21:54 +0530

    initial commit

---
+++ Chapter_03/program_09.java
@@ -0,0 +1,14 @@
+// 0. Formula Computation
+
+// Write a Java program to compute a specified formula.
+// Specified Formula :
+// 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+// Expected Output
+// 2.9760461760461765
+
+public class program_09 {
+    static void main (String [] args){
+        double  result = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(result);
+    }
+}
