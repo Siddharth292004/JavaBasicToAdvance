c945c131b20e8e0c2f21a6eae6ea8c0b9e33bc61
Siddharth292004 <sidharth9509@gmail.com>
2025-08-15 13:31:53 +0530

    initial commit

--- Chapter_03/program_02.java
+++ Chapter_03/program_02.java
@@ -9,7 +9,7 @@ public class program_02 {
     public static void main(String [] args){
         int num1 = 74;
         int num2 = 36;
-
-        System.out.println(num1 + num2);
+        int sum = num1 + num2;
+        System.out.println(sum);
     }
 }
