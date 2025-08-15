e286f7b1defd572f4708f2c7277f4792bda410bf
Siddharth292004 <sidharth9509@gmail.com>
2025-08-15 09:41:58 +0530

    initial commit

--- Chapter_01/Literals/SymbolTest.java
+++
@@ -1,7 +0,0 @@
-public class SymbolTest {
-    public static void main(String[] args) {
-        System.out.println("Heart: \u2764");
-        System.out.println("Arrow: \u2192");
-        System.out.println("Check: \u2713");
-    }
-}
