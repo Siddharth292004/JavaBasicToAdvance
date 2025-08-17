51dc8159abec9194020acc0014a7b5764a18d47f
Siddharth292004 <sidharth9509@gmail.com>
2025-08-17 16:12:59 +0530

    initial commit

--- Function/leapYear.java
+++ Function/leapYear.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class leapYear {
     public static boolean CheckLeapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0 )|| (year % 400 == 0)){
             return true;
         } else {
             return false;
         }
-
     }
 
     public static void main(String[] args) {
@@ -18,7 +17,7 @@ public class leapYear {
             if (CheckLeapYear(year)) {
                 System.out.println(year + " is a leap year.\n");
             } else {
-                System.out.println(year+" is not a leap year.\n");
+                System.out.println(year + " is not a leap year.\n");
             }
         } catch (Exception e) {
             System.err.println("Invalid input ! Please enter valid number.\n");
