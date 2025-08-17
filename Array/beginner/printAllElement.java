d3c4a41cb2e92087d07c8a61b7a0d4172c413dec
Siddharth292004 <sidharth9509@gmail.com>
2025-08-17 16:34:05 +0530

    initial commit

---
+++ Array/beginner/printAllElement.java
@@ -0,0 +1,28 @@
+package Array.beginner;
+
+import java.util.Scanner;
+
+public class printAllElement {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.err.print("Enter the size of the array: ");
+            int size = sc.nextInt();
+            int[] array = new int[size];
+
+            System.err.println("Enter the element in array:");
+            for (int i = 0; i < array.length; i++) {
+                System.err.print("arr[" + (i + 1) + "]");
+                array[i] = sc.nextInt();
+            }
+
+            System.out.println("\nElement of the array: ");
+            for (int i = 0; i < array.length; i++) {
+                System.err.println("arr[" + i + "] = " + array[i]);
+            }
+
+        } catch (Exception e) {
+            System.out.println("invalid input ! please enter valid input.\n");
+        }
+    }
+}
